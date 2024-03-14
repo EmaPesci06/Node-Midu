967d65ea518944c0238746cd1fa3d9a4748a178f
Emanuel Pesci <emanuelpesci@gmail.com>
2024-03-14 16:31:15 -0300

    clase 4

---
+++ clase_4/routes/movies.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+
+import { MovieController } from '../controllers/movies.js'
+
+export const movieRouter = Router()
+
+movieRouter.get('/', MovieController.getAll)
+movieRouter.post('/', MovieController.create)
+
+movieRouter.get('/:id', MovieController.getById)
+movieRouter.delete('/:id', MovieController.delete)
+movieRouter.patch('/:id', MovieController.update)
